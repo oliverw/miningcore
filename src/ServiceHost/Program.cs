357765feb28a76758886a0a6e15596c350efb2f5
Oliver Weichhold <oliver@weichhold.com>
2017-07-05 22:15:51 +0200

    Startup

--- src/ServiceHost/Program.cs
+++ src/ServiceHost/Program.cs
@@ -1,51 +1,88 @@
 ﻿using System;
-using System.Net;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using Microsoft.Extensions.Logging.Debug;
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using Transport;
-using Transport.LibUv;
 
-// https://nikhilm.github.io/uvbook/basics.html
+// - Read listener json config file
 
-namespace ServiceHost
+namespace MiningCore
 {
     class Program
     {
-        class EchoClient
-        {
-            public EchoClient(IConnection connection)
-            {
-                connection.Output.OnNext(System.Text.Encoding.UTF8.GetBytes("Ready.\n"));
-
-                connection.Input
-                    .ObserveOn(ThreadPoolScheduler.Instance)
-                    .Subscribe(x =>
-                {
-                    var msg = System.Text.Encoding.UTF8.GetString(x);
-
-                    for (int i = 0; i < 20000; i++)
-                    {
-                        var msg2 = $"{i} - You wrote: {msg}";
-                        connection.Output.OnNext(System.Text.Encoding.UTF8.GetBytes(msg2));
-                    }
-                });
-            }
-        }
+        public static IContainer Container { get; private set; }
+        private static AutofacServiceProvider serviceProvider;
 
         static void Main(string[] args)
         {
-            var logger = new DebugLogger("default");
-            var listener = (IEndpointDispatcher) new LibUvEndpointDispatcher(logger);
+            // Command-Line Options
+            var app = new CommandLineApplication(false);
+            app.FullName = "MiningCore - Mining Pool Engine";
+            app.ShortVersionGetter = () => $"(Build {Assembly.GetEntryAssembly().GetName().Version.Build})";
 
-            // handle ctrl+c
-            Console.CancelKeyPress += (sender, e) =>
+            var configFile = app.Option("-config|-c <configfile>", "Configuration File", CommandOptionType.SingleValue);
+            app.HelpOption("-? | -h | --help");
+
+            if (!configFile.HasValue())
             {
-                e.Cancel = true;
-                listener.Stop();
-            };
+                app.ShowHelp();
+                return;
+            }
 
-            listener.Start(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 57000), (transport => new EchoClient(transport)));
+            // Configure DI
+            var services  = new ServiceCollection()
+                .AddLogging();
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            builder.RegisterAssemblyModules(new[]
+            {
+                typeof(AutofacModule).GetTypeInfo().Assembly,
+            });
+
+            Container = builder.Build();
+
+            serviceProvider = new AutofacServiceProvider(Container);
+
+            // Congfigure logging
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>()
+                .AddNLog();
+
+            loggerFactory.ConfigureNLog("NLog.config");
+
+            var logger = Container.Resolve<ILogger<Program>>();
+
+            // Done
+            logger.LogInformation("MiningCore startup ...");
         }
     }
 }
+
+//class EchoClient
+//{
+//    public EchoClient(IConnection connection)
+//    {
+//        connection.Output.OnNext(System.Text.Encoding.UTF8.GetBytes("Ready.\n"));
+
+//        connection.Input
+//            .ObserveOn(ThreadPoolScheduler.Instance)
+//            .Subscribe(x =>
+//            {
+//                var msg = System.Text.Encoding.UTF8.GetString(x);
+
+//                for (int i = 0; i < 20000; i++)
+//                {
+//                    var msg2 = $"{i} - You wrote: {msg}";
+//                    connection.Output.OnNext(System.Text.Encoding.UTF8.GetBytes(msg2));
+//                }
+//            });
+//    }
+//}
