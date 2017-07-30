0fd47bd910142a7b4e9ea32d13559d43057920e1
Oliver Weichhold <oliver@weichhold.com>
2017-07-30 12:51:29 +0200

    API cleanup

---
+++ src/MiningForce/RestApi/ClusterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MiningForce.Configuration;
+using MiningForce.MininigPool;
+
+namespace MiningForce.RestApi
+{
+	[Route("api")]
+    public class PoolController : Controller
+    {
+	    [Route("config")]
+	    public ClusterConfig GetConfig()
+	    {
+		    return Program.ClusterConfig;
+	    }
+	}
+}
--- src/MiningForce/RestApi/PoolController.cs
+++ src/MiningForce/RestApi/PoolController.cs
@@ -8,31 +8,24 @@ using MiningForce.MininigPool;
 
 namespace MiningForce.RestApi
 {
-	[Route("api")]
-    public class PoolController : Controller
+	[Route("api/pool")]
+    public class ClusterController : Controller
     {
-	    [Route("config")]
-	    public ClusterConfig GetConfig()
-	    {
-		    return Program.ClusterConfig;
-	    }
-
-	    [Route("pool")]
-	    public string[] GetPools()
+	    public string[] Index()
 	    {
 		    return Program.Pools.Keys
 			    .Select(x => x.Id)
 			    .ToArray();
 	    }
 
-	    [Route("pool/{poolId}/config")]
+	    [Route("{poolId}/config")]
 	    public PoolConfig GetPoolConfig(string poolId)
 	    {
 		    return Program.Pools.Keys.FirstOrDefault(x => x.Id == poolId);
 	    }
 
-	    [Route("pool/{poolId}/stats")]
-	    public dynamic GetPoolStats(string poolId)
+	    [Route("{poolId}/stats")]
+	    public object GetPoolStats(string poolId)
 	    {
 		    var poolConfig = Program.Pools.Keys.FirstOrDefault(x => x.Id == poolId);
 		    if (poolConfig == null)
@@ -40,7 +33,7 @@ namespace MiningForce.RestApi
 
 		    var pool = Program.Pools[poolConfig];
 
-			return new { pool = pool.PoolStats, network = pool.NetworkStats };
+		    return new { pool = pool.PoolStats, network = pool.NetworkStats };
 	    }
 	}
 }
