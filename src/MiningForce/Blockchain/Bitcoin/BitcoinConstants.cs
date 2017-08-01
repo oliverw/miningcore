b542b61337baacb30b89232b3225b8ec48573a36
Oliver Weichhold <oliver@weichhold.com>
2017-08-01 18:00:11 +0200

    Cleanup

--- src/MiningForce/Blockchain/Bitcoin/BitcoinConstants.cs
+++ src/MiningForce/Blockchain/Bitcoin/BitcoinConstants.cs
@@ -47,6 +47,20 @@ namespace MiningForce.Blockchain.Bitcoin
 		public const decimal SatoshisPerBitcoin = 100000000;
 	}
 
+	public class KnownAddresses
+	{
+		public static readonly Dictionary<CoinType, string> DevFeeAddresses = new Dictionary<CoinType, string>
+		{
+			{CoinType.BTC, "17QnVor1B6oK1rWnVVBrdX9gFzVkZZbhDm"},
+			{CoinType.LTC, "LTK6CWastkmBzGxgQhTTtCUjkjDA14kxzC"},
+			{CoinType.DOGE, "DGDuKRhBewGP1kbUz4hszNd2p6dDzWYy9Q"},
+			{CoinType.NMC, "NDSLDpFEcTbuRVcWHdJyiRZThVAcb5Z79o"},
+			{CoinType.DGB, "DAFtYMGVdNtqHJoBGg2xqZZwSuYAaEs2Bn"},
+			{CoinType.PPC, "PE8RH6HAvi8sqYg47D58TeKTjyeQFFHWR2"},
+			{CoinType.VIA, "Vc5rJr2QdA2yo1jBoqYUAH7T59uBh2Vw5q"},
+		};
+	}
+
 	public class BitcoinCoinsMetaData : CoinMetadataAttribute
 	{
 		public BitcoinCoinsMetaData() : base(
