5d5634c90e8b791b87d2630a4a41dd736d373e54
Oliver Weichhold <oliver@weichhold.com>
2017-08-06 07:29:39 +0200

    Cleanup

--- src/MiningForce/Blockchain/Bitcoin/BitcoinConstants.cs
+++ src/MiningForce/Blockchain/Bitcoin/BitcoinConstants.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Numerics;
-using System.Text;
+﻿using System.Collections.Generic;
 using MiningForce.Configuration;
 
 namespace MiningForce.Blockchain.Bitcoin
--- src/MiningForce/Blockchain/Monero/MoneroConstants.cs
+++ src/MiningForce/Blockchain/Monero/MoneroConstants.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.RegularExpressions;
-using MiningForce.Extensions;
 using NBitcoin.BouncyCastle.Math;
 
 namespace MiningForce.Blockchain.Monero
@@ -13,7 +12,14 @@ namespace MiningForce.Blockchain.Monero
 	public class MoneroConstants
 	{
 		public const string WalletDaemonCategory = "wallet";
-
+		public const int AddressLength = 95;
+		public const string DaemonRpcLocation = "json_rpc";
+		public const string DaemonRpcDigestAuthRealm = "monero_rpc";
+		public const char MainNetAddressPrefix = '4';
+		public const char TestNetAddressPrefix = '9';
+		public static readonly Regex RegexValidNonce = new Regex("^[0-9a-f]{8}$", RegexOptions.Compiled);
+		public static readonly BigInteger Diff1 = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
+		public const double DifficultyNormalizationFactor = 14226363340d;
 		public const int InstanceIdSize = 3;
 		public const int ExtraNonceSize = 4;
 
@@ -23,19 +29,5 @@ namespace MiningForce.Blockchain.Monero
 
 		// Offset to nonce in block blob
 		public const int BlobNonceOffset = 39;
-
-		public const int AddressLength = 95;
-
-		public const string DaemonRpcLocation = "json_rpc";
-		public const string DaemonRpcDigestAuthRealm = "monero_rpc";
-
-		public const char MainNetAddressPrefix = '4';
-		public const char TestNetAddressPrefix = '9';
-
-		public static readonly Regex RegexValidNonce = new Regex("^[0-9a-f]{8}$", RegexOptions.Compiled);
-
-		public static readonly BigInteger Diff1 = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16);
-
-		public const double DifficultyNormalizationFactor = 14226363340d;
 	}
 }
