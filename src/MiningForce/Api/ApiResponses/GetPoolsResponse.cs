1d24594f7cf4e3188444e92d255b53989839fcc7
Oliver Weichhold <oliver@weichhold.com>
2017-08-07 06:14:31 +0200

    Cleanup

--- src/MiningForce/Api/ApiResponses/GetPoolsResponse.cs
+++ src/MiningForce/Api/ApiResponses/GetPoolsResponse.cs
@@ -7,16 +7,17 @@ namespace MiningForce.Api.ApiResponses
 {
 	public class PoolInfo
 	{
+		// Configuration Properties directly mapping to PoolConfig (omitting security relevant fields)
 		public string Id { get; set; }
-		public bool Enabled { get; set; }
 		public CoinConfig Coin { get; set; }
 		public Dictionary<int, PoolEndpoint> Ports { get; set; }
 		public PoolPaymentProcessingConfig PaymentProcessing { get; set; }
 		public PoolBanningConfig Banning { get; set; }
-		public RewardRecipient[] RewardRecipients { get; set; }
 		public int ClientConnectionTimeout { get; set; }
 		public int JobRebroadcastTimeout { get; set; }
 		public int BlockRefreshInterval { get; set; }
+
+		// Stats
 		public PoolStats PoolStats { get; set; }
 		public BlockchainStats NetworkStats { get; set; }
 	}
