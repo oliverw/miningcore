30f50f48bcdac593b11b8823600fd822cbb04adc
Oliver Weichhold <oliver@weichhold.com>
2017-11-18 20:41:30 +0100

    Improved Bitcoin Extranonce handling and made it more ASIC miner friendly be decreasing it by 8 bits

--- src/MiningCore/Blockchain/Bitcoin/BitcoinExtraNonceProvider.cs
+++ src/MiningCore/Blockchain/Bitcoin/BitcoinExtraNonceProvider.cs
@@ -1,27 +1,26 @@
-﻿/* 
+﻿/*
 Copyright 2017 Coin Foundry (coinfoundry.org)
 Authors: Oliver Weichhold (oliver@weichhold.com)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial 
+The above copyright notice and this permission notice shall be included in all copies or substantial
 portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
-LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 using System;
-using System.Runtime.InteropServices;
 using System.Security.Cryptography;
-using MiningCore.Extensions;
+using System.Threading;
 
 namespace MiningCore.Blockchain.Bitcoin
 {
@@ -29,29 +28,36 @@ namespace MiningCore.Blockchain.Bitcoin
     {
         public BitcoinExtraNonceProvider()
         {
-            uint instanceId;
+            int instanceId;
 
             using(var rng = RandomNumberGenerator.Create())
             {
                 var bytes = new byte[4];
                 rng.GetNonZeroBytes(bytes);
-                instanceId = BitConverter.ToUInt32(bytes, 0);
+                instanceId = BitConverter.ToInt32(bytes, 0);
             }
 
-            counter = instanceId << 27;
+            var mask = (1 << (ExtranonceBytes * 8)) - 1;
+            counter = Math.Abs(instanceId & mask);
         }
 
-        private uint counter;
-
-        public static byte[] PlaceHolder { get; } = "f000000ff111111f".HexToByteArray();
-
-        public static int Size => PlaceHolder.Length - Marshal.SizeOf(typeof(uint));
+        private int counter;
+        public const int ExtranonceBytes = 3; // 3 Byte = 24 Bit
+        public const int PlaceHolderLength = 8;
+        private const int NonceMax = 1 << (ExtranonceBytes * 8);
+        private readonly string stringFormat = "x" + ExtranonceBytes * 2;
+        public const int Size = PlaceHolderLength - ExtranonceBytes;
 
         #region IExtraNonceProvider
 
         public string Next()
         {
-            return (++counter).ToBigEndian().ToStringHex8();
+            Interlocked.Increment(ref counter);
+            Interlocked.CompareExchange(ref counter, 0, NonceMax);
+
+            // encode to hex
+            var result = counter.ToString(stringFormat);
+            return result;
         }
 
         #endregion // IExtraNonceProvider
