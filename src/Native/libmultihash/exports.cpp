a93be434522623c28a62d3733d11049ad85f1dea
Oliver Weichhold <oliver@weichhold.com>
2018-01-24 10:04:44 +0100

    Neoscrypt update

--- src/Native/libmultihash/exports.cpp
+++ src/Native/libmultihash/exports.cpp
@@ -73,7 +73,7 @@ extern "C" MODULE_API void x15_export(const char* input, char* output, uint32_t 
 	x15_hash(input, output, input_len);
 }
 
-extern "C" MODULE_API void neoscrypt_export(const char* input, char* output, uint32_t profile)
+extern "C" MODULE_API void neoscrypt_export(const unsigned char* input, unsigned char* output, uint32_t profile)
 {
 	neoscrypt(input, output, profile);
 }
