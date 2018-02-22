b1705d66c51e3345660822dca24702c327c0ef88
Oliver Weichhold <oliver@weichhold.com>
2018-02-22 15:10:35 +0100

    WIP

--- src/MiningCore/Persistence/Postgres/Repositories/StatsRepository.cs
+++ src/MiningCore/Persistence/Postgres/Repositories/StatsRepository.cs
@@ -218,22 +218,24 @@ namespace MiningCore.Persistence.Postgres.Repositories
                     SharesPerSecond = y.SharesPerSecond
                 })
             })
-            .ToDictionary(x=> x.Created.UnixTimestamp(), x=> x);
+            .ToArray();
+            //.ToDictionary(x=> x.Created.UnixTimestamp(), x=> x);
 
-            // fill in blanks
-            var result = new List<WorkerPerformanceStatsContainer>();
+            //// fill in blanks
+            //var result = new List<WorkerPerformanceStatsContainer>();
 
-            for (var i = 0; i < 24; i++)
-            {
-                if(tmp.TryGetValue(end.UnixTimestamp(), out var item))
-                    result.Insert(0, item);
-                else
-                    result.Add(new WorkerPerformanceStatsContainer { Created = end });
+            //for (var i = 0; i < 24; i++)
+            //{
+            //    if(tmp.TryGetValue(end.UnixTimestamp(), out var item))
+            //        result.Insert(0, item);
+            //    else
+            //        result.Add(new WorkerPerformanceStatsContainer { Created = end });
 
-                end = end.AddHours(-1);
-            }
+            //    end = end.AddHours(-1);
+            //}
 
-            return result.ToArray();
+            //return result.ToArray();
+            return tmp;
         }
 
         public WorkerPerformanceStatsContainer[] GetMinerPerformanceBetweenDaily(IDbConnection con, string poolId, string miner, DateTime start, DateTime end)
@@ -259,22 +261,25 @@ namespace MiningCore.Persistence.Postgres.Repositories
                     SharesPerSecond = y.SharesPerSecond
                 })
             })
-            .ToDictionary(x => x.Created.UnixTimestamp(), x => x);
+            .ToArray();
+            //.ToDictionary(x => x.Created.UnixTimestamp(), x => x);
 
-            // fill in blanks
-            var result = new List<WorkerPerformanceStatsContainer>();
+            //// fill in blanks
+            //var result = new List<WorkerPerformanceStatsContainer>();
 
-            for (var i = 0; i < 30; i++)
-            {
-                if (tmp.TryGetValue(end.UnixTimestamp(), out var item))
-                    result.Insert(0, item);
-                else
-                    result.Add(new WorkerPerformanceStatsContainer { Created = end });
+            //for (var i = 0; i < 30; i++)
+            //{
+            //    if (tmp.TryGetValue(end.UnixTimestamp(), out var item))
+            //        result.Insert(0, item);
+            //    else
+            //        result.Add(new WorkerPerformanceStatsContainer { Created = end });
 
-                end = end.AddDays(-1);
-            }
+            //    end = end.AddDays(-1);
+            //}
 
-            return result.ToArray();
+            //return result.ToArray();
+
+            return tmp;
         }
 
         public MinerWorkerPerformanceStats[] PagePoolMinersByHashrate(IDbConnection con, string poolId, DateTime from, int page, int pageSize)
