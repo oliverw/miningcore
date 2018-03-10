5020e2fc4a270d6eee63d9a20e4b2bb7a4d96e62
Oliver Weichhold <oliver@weichhold.com>
2018-03-10 12:36:32 +0100

    WIP

--- src/MiningCore/Blockchain/Ethereum/EthereumJobManager.cs
+++ src/MiningCore/Blockchain/Ethereum/EthereumJobManager.cs
@@ -189,7 +189,7 @@ namespace MiningCore.Blockchain.Ethereum
         private EthereumBlockTemplate AssembleBlockTemplate(Block block, string[] work)
         {
             // only parity returns the 4th element (block height)
-            if (work.Length < 3)
+            if (work.Length < 4)
             {
                 logger.Error(() => $"[{LogCat}] Error(s) refreshing blocktemplate: getWork did not return blockheight. Are you really connected to a Parity daemon?");
                 return null;
