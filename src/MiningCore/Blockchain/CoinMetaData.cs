b78bac06f1d7fb85289b5c4ae44d17fb89307047
Oliver Weichhold <oliver@weichhold.com>
2018-04-12 15:29:49 +0200

    RVN, PGN address meta data

--- src/MiningCore/Blockchain/CoinMetaData.cs
+++ src/MiningCore/Blockchain/CoinMetaData.cs
@@ -59,6 +59,8 @@ namespace MiningCore.Blockchain
             { CoinType.PAK, new Dictionary<string, string> { { string.Empty, "https://chainz.cryptoid.info/pak/block.dws?{BlockHeightPH}.htm" } }},
             { CoinType.FLO, new Dictionary<string, string> { { string.Empty, "https://florincoin.info/block/{0}" } }},
             { CoinType.CANN, new Dictionary<string, string> { { string.Empty, $"https://chainz.cryptoid.info/cann/block.dws?{BlockHeightPH}.htm" }}},
+            { CoinType.RVN, new Dictionary<string, string> { { string.Empty, $"http://explorer.threeeyed.info/block/{BlockHashPH}" }}},
+            { CoinType.PGN, new Dictionary<string, string> { { string.Empty, $"http://explorer.pigeoncoin.org/block/{BlockHashPH}" }}},
         };
 
         public static readonly Dictionary<CoinType, string> TxInfoLinks = new Dictionary<CoinType, string>
@@ -96,6 +98,8 @@ namespace MiningCore.Blockchain
             { CoinType.PAK, "https://chainz.cryptoid.info/pak/tx.dws?{0}.htm" },
             { CoinType.FLO, "https://florincoin.info/tx/{0}" },
             { CoinType.CANN, "https://chainz.cryptoid.info/cann/tx.dws?{0}.htm" },
+            { CoinType.RVN, "http://explorer.threeeyed.info/tx/{0}" },
+            { CoinType.PGN, "http://explorer.pigeoncoin.org/tx/{0}" },
         };
 
         public static readonly Dictionary<CoinType, string> AddressInfoLinks = new Dictionary<CoinType, string>
@@ -130,6 +134,8 @@ namespace MiningCore.Blockchain
             { CoinType.PAK, "https://chainz.cryptoid.info/pak/address.dws?{0}.htm" },
             { CoinType.FLO, "https://florincoin.info/address/{0}" },
             { CoinType.CANN, "https://chainz.cryptoid.info/cann/address.dws?{0}.htm" },
+            { CoinType.RVN, "http://explorer.threeeyed.info/address/{0}" },
+            { CoinType.PGN, "http://explorer.pigeoncoin.org/address/{0}" },
         };
 
         private const string Ethash = "Ethash";
