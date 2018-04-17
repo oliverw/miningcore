ee3780c71287af328949f8d1961492e35cb7c62f
Oliver Weichhold <oliver@weichhold.com>
2018-04-17 11:24:22 +0200

    Nuget package updates

--- src/MiningCore/Blockchain/Bitcoin/BitcoinJob.cs
+++ src/MiningCore/Blockchain/Bitcoin/BitcoinJob.cs
@@ -273,7 +273,9 @@ namespace MiningCore.Blockchain.Bitcoin
             // build merkle-root
             var merkleRoot = mt.WithFirst(coinbaseHash);
 
+            #pragma warning disable 618
             var blockHeader = new BlockHeader
+            #pragma warning restore 618
             {
                 Version = (int) BlockTemplate.Version,
                 Bits = new Target(Encoders.Hex.DecodeData(BlockTemplate.Bits)),
