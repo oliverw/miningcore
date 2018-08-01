cfeeb1a79939d7c8b865c268a3b7b3f31a8c919f
Caleb Call <caleb@si-senor.local>
2018-08-01 17:14:31 -0600

    Missed lyra2z changes in exports file

--- src/Native/libmultihash/exports.cpp
+++ src/Native/libmultihash/exports.cpp
@@ -41,6 +41,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "jh.h"
 #include "c11.h"
 #include "Lyra2RE.h"
+#include "Lyra2Z.h"
 #include "Lyra2.h"
 #include "x16r.h"
 #include "x16s.h"
@@ -187,6 +188,11 @@ extern "C" MODULE_API void lyra2re_export(const char* input, char* output)
 extern "C" MODULE_API void lyra2rev2_export(const char* input, char* output)
 {
 	lyra2re2_hash(input, output);
+}
+
+extern "C" MODULE_API void lyra2z_export(const char* input, char* output)
+{
+	lyra2z_hash(input, output);
 }
 
 extern "C" MODULE_API void x16r_export(const char* input, char* output, uint32_t input_len)
