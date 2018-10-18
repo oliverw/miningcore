bf33448b5c3732654dbe91d6e5443f9cb3272414
Oliver Weichhold <oliver@weichhold.com>
2018-10-18 22:16:20 +0200

    WIP

--- src/Native/libcryptonight/crypto/Asm.h
+++ src/Native/libcryptonight/crypto/Asm.h
@@ -5,7 +5,6 @@
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
- * Copyright 2018      Lee Clagett <https://github.com/vtnerd>
  * Copyright 2016-2018 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
@@ -22,18 +21,30 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef XMRIG_CRYPTONIGHT_H
-#define XMRIG_CRYPTONIGHT_H
+#ifndef XMRIG_ASM_H
+#define XMRIG_ASM_H
 
 
-#include <stddef.h>
-#include <stdint.h>
+#include "common/xmrig.h"
+#include "rapidjson/fwd.h"
 
 
-struct cryptonight_ctx {
-    alignas(16) uint8_t state[224];
-    alignas(16) uint8_t *memory;
+namespace xmrig {
+
+
+class Asm
+{
+public:
+    static Assembly parse(const char *assembly, Assembly defaultValue = ASM_AUTO);
+    static Assembly parse(const rapidjson::Value &value, Assembly defaultValue = ASM_AUTO);
+    static const char *toString(Assembly assembly);
+    static rapidjson::Value toJSON(Assembly assembly);
+
+    inline static Assembly parse(bool enable) { return enable ? ASM_AUTO : ASM_NONE; }
 };
 
 
-#endif /* XMRIG_CRYPTONIGHT_H */
+} /* namespace xmrig */
+
+
+#endif /* XMRIG_ASM_H */
