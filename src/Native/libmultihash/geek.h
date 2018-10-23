9443c12a3687a760694923f6d1c9a73fa1030108
calebcall <calebcall@me.com>
2018-10-23 12:05:23 -0700

    geek algo and geekcash coin template (#435)
    * GEEK algo and geekcash coin template

---
+++ src/Native/libmultihash/geek.h
@@ -0,0 +1,16 @@
+#ifndef GEEK_H
+#define GEEK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void geek_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
