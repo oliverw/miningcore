ab8b00595d3686882a5d3835335398e73f49a4c7
Caleb Call <caleb@si-senor.local>
2018-10-22 18:09:13 -0600

    GEEK algo and geekcash coin template

---
+++ src/Miningcore/Crypto/Hashing/Algorithms/Geek.cs
@@ -0,0 +1,23 @@
+using System;
+using Miningcore.Contracts;
+using Miningcore.Native;
+
+namespace Miningcore.Crypto.Hashing.Algorithms
+{
+    public unsafe class geek : IHashAlgorithm
+    {
+        public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
+        {
+            Contract.Requires<ArgumentException>(data.Length == 80, $"{nameof(data)} must be exactly 80 bytes long");
+            Contract.Requires<ArgumentException>(result.Length >= 32, $"{nameof(result)} must be greater or equal 32 bytes");
+
+            fixed (byte* input = data)
+            {
+                fixed (byte* output = result)
+                {
+                    LibMultihash.geek(input, output, (uint) data.Length);
+                }
+            }
+        }
+    }
+}
