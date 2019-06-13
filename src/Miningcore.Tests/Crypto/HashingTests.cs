a1998414b4a7cf3f4f7a64dba2f5da3a72e513a8
Konstantin <twozo.pw@gmail.com>
2019-06-14 01:23:27 +0300

    Update HashingTests.cs

--- src/Miningcore.Tests/Crypto/HashingTests.cs
+++ src/Miningcore.Tests/Crypto/HashingTests.cs
@@ -214,12 +214,16 @@ namespace Miningcore.Tests.Crypto
         [Fact]
         public void X16RT_Hash()
         {
+            //# Block 5000 (ce9e64ee2f9832f9ff6a8b042d0aa66f2e0aca6e8a5958051c141f27b5e68a3b)
+            string blockHeader = "0000002049963e9e46701e28e2dd44f61dea241ba3a457979fa3b378822c6cc24457bc2500a0da644814e2a43cc163f3f37590982e1e2f5ac45d1de2f8222750fea1c844f8d2315c824d111b120979e30000";
+            byte[] headerHeaderBytes = blockHeader.HexToByteArray();
+
             var hasher = new X16RT();
             var hash = new byte[32];
-            hasher.Digest(testValue, hash);
+            hasher.Digest(headerHeaderBytes, hash);
             var result = hash.ToHexString();
 
-            Assert.Equal("5ef7d66548446d1e71190ff06358b1f63169434e8820af738b32ada4a91f1fe2", result); 
+            Assert.Equal("40abc3e30ba0552a7910c0acd5b46a5f8dbf287e0108ee679d59070000000000", result);
         }
 
         [Fact]
