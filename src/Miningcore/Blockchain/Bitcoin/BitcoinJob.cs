fe6750cb3c14468b1338c0b70d756a5cabbf3532
akshaynexus <akshaycm@hotmail.com>
2020-04-23 22:13:58 +0000

    Add support for current PGN Mainnet

--- src/Miningcore/Blockchain/Bitcoin/BitcoinJob.cs
+++ src/Miningcore/Blockchain/Bitcoin/BitcoinJob.cs
@@ -263,7 +263,10 @@ namespace Miningcore.Blockchain.Bitcoin
         protected virtual Transaction CreateOutputTransaction()
         {
             rewardToPool = new Money(BlockTemplate.CoinbaseValue, MoneyUnit.Satoshi);
-
+            //Now check if we need to pay founder fees Re PGN pre-dash fork
+            if(coin.HasFounderFee)
+                rewardToPool = CreateFounderOutputs(tx,rewardToPool);
+            
             var tx = Transaction.Create(network);
 
             tx.Outputs.Add(rewardToPool, poolAddressDestination);
