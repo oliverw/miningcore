885a0addb7076987ee160ec007505f03f32f1810
Konstantin <twozo.pw@gmail.com>
2020-12-23 23:31:24 +0300

    Update ClusterConfig.cs

--- src/Miningcore/Configuration/ClusterConfig.cs
+++ src/Miningcore/Configuration/ClusterConfig.cs
@@ -348,7 +348,7 @@ namespace Miningcore.Configuration
     {
         // ReSharper disable once InconsistentNaming
         PPLNS = 1,
-        Solo
+        SOLO = 2
     }
 
     public partial class ClusterLoggingConfig
