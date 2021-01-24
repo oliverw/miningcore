5b89154226113a4be4946e7438109dd54beb4d8d
minernl - miningcore.com <32671091+minernl@users.noreply.github.com>
2021-01-24 19:48:52 +0100

    Update linux-build.sh

--- src/Miningcore/linux-build.sh
+++ src/Miningcore/linux-build.sh
@@ -3,6 +3,7 @@ echo ""
 echo "The following dev-dependencies must be installed"
 echo "Ubuntu: apt-get install git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5"
 echo ""
+sudo apt-get install git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5
 BUILDIR=${1:-../../build}
 echo "Building into $BUILDIR"
 dotnet publish -c Release --framework netcoreapp3.1 -o $BUILDIR
