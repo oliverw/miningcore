7cc91be035ed0ee1048d494eb2cd90cc250a9fc0
MinerNL <MinerNL@outlook.com>
2021-02-08 00:46:43 +0100

    tst

--- src/Miningcore/Serialization/HexToIntegralTypeJsonConverter.cs
+++ src/Miningcore/Serialization/HexToIntegralTypeJsonConverter.cs
@@ -22,12 +22,15 @@ namespace Miningcore.Serialization
         {
             Console.WriteLine($"WRITE writer: {writer}");
             Console.WriteLine($"WRITE value: {value}");
-            Console.WriteLine($"WRITE serializer: {serializer}");
 
             if(value == null)
                 writer.WriteValue("null");
             else
+            {
+                Console.WriteLine($"WRITE converted 0x{value:x}");
                 writer.WriteValue($"0x{value:x}");
+            }
+                
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
@@ -36,7 +39,6 @@ namespace Miningcore.Serialization
 
             Console.WriteLine($"READ str: {str}");
             Console.WriteLine($"READ Type: {objectType}");
-            Console.WriteLine($"READ serializer: {serializer}");
 
             if(string.IsNullOrEmpty(str))
                 return default(T);
@@ -45,16 +47,15 @@ namespace Miningcore.Serialization
                 str = str.Substring(2);
 
             if(typeof(T) == typeof(BigInteger))
-                return BigInteger.Parse(str, NumberStyles.HexNumber);
-
-            //if(typeof(T) == typeof(BigInteger))
-            //    return BigInteger.Parse("0" + str, NumberStyles.HexNumber);
+                return BigInteger.Parse("0" + str, NumberStyles.HexNumber);
 
             if(typeof(T) == typeof(uint256))
                 return new uint256(str.HexToReverseByteArray());
 
-            var val = ulong.Parse(str, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-            //var val = ulong.Parse("0" + str, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            var val = ulong.Parse("0" + str, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"READ val: {val}");
+
             return Convert.ChangeType(val, underlyingType ?? typeof(T));
         }
     }
