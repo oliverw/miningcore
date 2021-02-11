e405bb2f7530c7716193fc5b8667432ead3bb17b
MinerNL <MinerNL@outlook.com>
2021-02-11 02:39:01 +0100

    note wip

--- src/Miningcore/Blockchain/Cryptonote/DaemonResponses/GetBlockHeaderResponse.cs
+++ src/Miningcore/Blockchain/Cryptonote/DaemonResponses/GetBlockHeaderResponse.cs
@@ -1,55 +1,111 @@
 /*
-Copyright 2017 Coin Foundry (coinfoundry.org)
-Authors: Oliver Weichhold (oliver@weichhold.com)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial
-portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+MiningCore 2.0
+Copyright 2021 MinerNL (Miningcore.com)
 */
 
 using Newtonsoft.Json;
 
 namespace Miningcore.Blockchain.Cryptonote.DaemonResponses
 {
-    public class BlockHeader
-    {
-        public long Difficulty { get; set; }
-        public long Depth { get; set; }
-        public uint Height { get; set; }
-        public string Hash { get; set; }
-        public string Nonce { get; set; }
-        public ulong Reward { get; set; }
-        public ulong Timestamp { get; set; }
-
-        [JsonProperty("major_version")]
-        public uint MajorVersion { get; set; }
-
-        [JsonProperty("minor_version")]
-        public uint MinorVersion { get; set; }
-
-        [JsonProperty("prev_hash")]
-        public string PreviousBlockhash { get; set; }
-
-        [JsonProperty("orphan_status")]
-        public bool IsOrphaned { get; set; }
-    }
+    /*
+        get_last_block_header
+        Block header information for the most recent block is easily retrieved with this method.
+        No inputs are needed
+        Alias: getlastblockheader
+    */
 
     public class GetBlockHeaderResponse
     {
+        /// <summary>
+        ///  A structure containing block header information
+        /// </summary>
         [JsonProperty("block_header")]
         public BlockHeader BlockHeader { get; set; }
 
+        /// <summary>
+        /// General RPC error code. "OK" means everything looks good
+        /// </summary>
         public string Status { get; set; }
+
+        /// <summary>
+        /// States if the result is obtained using the bootstrap mode, and is therefore not trusted (true), or when the daemon is fully synced (false)
+        /// </summary>
+        public bool Untrusted { get; set; }
     }
+
+    public class BlockHeader
+    {
+        /// <summary>
+        /// The block size in bytes
+        /// </summary>
+        [JsonProperty("block_size")]
+        public uint BlockSize {get; set;}
+
+        /// <summary>
+        /// The number of blocks succeeding this block on the blockchain. A larger number means an older block
+        /// </summary>
+        public long Depth { get; set; }
+
+        /// <summary>
+        /// The strength of the Monero network based on mining power
+        /// </summary>
+        public uint Difficulty { get; set; }
+
+        /// <summary>
+        /// The hash of this block
+        /// </summary>
+        public string Hash { get; set; }
+
+        /// <summary>
+        /// The number of blocks preceding this block on the blockchain
+        /// </summary>
+        public uint Height { get; set; }
+
+        /// <summary>
+        /// The major version of the monero protocol at this block height
+        /// </summary>
+        [JsonProperty("major_version")]
+        public uint MajorVersion { get; set; }
+
+        /// <summary>
+        /// The minor version of the monero protocol at this block height
+        /// </summary>
+        [JsonProperty("minor_version")]
+        public uint MinorVersion { get; set; }
+
+        /// <summary>
+        /// a cryptographic random one-time number used in mining a Monero block
+        /// </summary>
+        public string Nonce { get; set; }
+
+        /// <summary>
+        /// Number of transactions in the block, not counting the coinbase tx
+        /// </summary>
+        [JsonProperty("num_txes")]
+        public uint TxCount { get; set; }
+
+        /// <summary>
+        /// Usually false. If true, this block is not part of the longest chain
+        /// </summary>
+        [JsonProperty("orphan_status")]
+        public bool IsOrphaned { get; set; }
+
+        /// <summary>
+        /// The hash of the block immediately preceding this block in the chain
+        /// </summary>
+        [JsonProperty("prev_hash")]
+        public string PreviousBlockhash { get; set; }
+
+        /// <summary>
+        ///  The amount of new atomic units generated in this block and rewarded to the miner. Note: 1 XMR = 1e12 atomic units
+        /// </summary>
+        public ulong Reward { get; set; }
+
+        /// <summary>
+        /// The unix time at which the block was recorded into the blockchain
+        /// </summary>
+        public ulong Timestamp { get; set; }
+
+    }
+
 }
--- src/Miningcore/Blockchain/Cryptonote/DaemonResponses/GetBlockTemplateResponse.cs
+++ src/Miningcore/Blockchain/Cryptonote/DaemonResponses/GetBlockTemplateResponse.cs
@@ -1,41 +1,73 @@
 /*
-Copyright 2017 Coin Foundry (coinfoundry.org)
-Authors: Oliver Weichhold (oliver@weichhold.com)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial
-portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+MiningCore 2.0
+Copyright 2021 MinerNL (Miningcore.com)
 */
 
 using Newtonsoft.Json;
 
 namespace Miningcore.Blockchain.Cryptonote.DaemonResponses
 {
+
+     /*
+        get_block_template
+        Get a block template on which mining a new block.
+        Alias: getblocktemplate.
+        Inputs:
+            wallet_address - string; Address of wallet to receive coinbase transactions if block is successfully mined.
+            reserve_size - unsigned int; Reserve size.
+     */
+
     public class GetBlockTemplateResponse
     {
+        /// <summary>
+        /// Blob on which to try to mine a new block
+        /// </summary>
         [JsonProperty("blocktemplate_blob")]
         public string Blob { get; set; }
 
+        /// <summary>
+        /// Blob on which to try to find a valid nonce
+        /// </summary>
+        [JsonProperty("blockhashing_blob")]
+        public string HashBlob { get; set; }
+
+        /// <summary>
+        /// Difficulty of next block
+        /// </summary>
         public long Difficulty { get; set; }
+
+        /// <summary>
+        /// Coinbase reward expected to be received if block is successfully mined
+        /// </summary>
+        [JsonProperty("expected_reward")]
+        public uint ExpectedReward { get; set; }
+
+        /// <summary>
+        /// Height on which to mine
+        /// </summary>
         public uint Height { get; set; }
 
+        /// <summary>
+        /// Hash of the most recent block on which to mine the next block
+        /// </summary>
         [JsonProperty("prev_hash")]
         public string PreviousBlockhash { get; set; }
 
+        /// <summary>
+        /// Reserved offset
+        /// </summary>
         [JsonProperty("reserved_offset")]
-        public int ReservedOffset { get; set; }
+        public uint ReservedOffset { get; set; }
 
+        /// <summary>
+        /// General RPC error code. "OK" means everything looks good
+        /// </summary>
         public string Status { get; set; }
+
+        /// <summary>
+        /// States if the result is obtained using the bootstrap mode, and is therefore not trusted (true), or when the daemon is fully synced (false)
+        /// </summary>
+        public bool Untrusted { get; set; }
+
     }
 }
