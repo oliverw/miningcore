28a1f7907ac3624a10e23828f1ce080405bc0077
MinerNL <MinerNL@outlook.com>
2021-02-21 22:29:08 +0100

    .NET core 2.1 support

--- src/Miningcore/linux-x64-build-netcore21.sh
+++ src/Miningcore/linux-x64-build-netcore21.sh
@@ -4,6 +4,14 @@ echo "The following dev-dependencies must be installed"
 echo "Ubuntu: apt-get install git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5"
 echo ""
 sudo apt-get install git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5
+
+echo .
+echo "Installing dotnet SDK core 2.1"
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-2.1
+
 BUILDIR=${1:-../../build}
 echo "Building into $BUILDIR"
-dotnet publish -c Release --framework netcoreapp2.1 -o $BUILDIR
+dotnet publish -c Release --framework netcoreapp2.1 --runtime linux-x64 --self-contained true -o $BUILDIR
