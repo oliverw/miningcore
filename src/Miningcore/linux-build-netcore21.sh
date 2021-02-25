49a5bc17efecb9fe5478ca114019a9f65213ecb0
minernl - miningcore.com <32671091+minernl@users.noreply.github.com>
2021-02-25 10:31:42 +0100

    Update linux-build-netcore21.sh

--- src/Miningcore/linux-build-netcore21.sh
+++ src/Miningcore/linux-build-netcore21.sh
@@ -4,12 +4,21 @@ echo "The following dev-dependencies must be installed"
 echo "Ubuntu: apt-get install git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5"
 echo ""
 sudo apt-get install git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5
+
 echo ""
-echo "Installing dotnet SDK core 2.1"
+echo "Add dotnet SDK Install source"
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-2.1
+  sudo apt-get update
+  
+echo ""
+echo "Installing dotnet SDK core 2.1"
+sudo apt-get install -y dotnet-sdk-2.1
+
+echo ""
+echo "Installing dotnet SDK core 3.1"
+sudo apt-get install -y dotnet-sdk-3.1
+
 echo ""
 echo "Building Miningcore 2.0 (.NET core 2.1)"
 BUILDIR=${1:-../../build}
