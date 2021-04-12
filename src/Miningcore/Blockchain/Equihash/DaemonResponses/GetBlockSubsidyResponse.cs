a205f67e305d1c22a0e6460031fd9c2b8a63d800
Maxim Borislav <maximb1124@gmail.com>
2021-04-12 14:40:12 +0900

    bug:fix fundingstream for zcash

--- src/Miningcore/Blockchain/Equihash/DaemonResponses/GetBlockSubsidyResponse.cs
+++ src/Miningcore/Blockchain/Equihash/DaemonResponses/GetBlockSubsidyResponse.cs
@@ -17,7 +17,9 @@ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMA
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
+using System;
+using System.Collections.Generic;
+using System.Collections;
 using Newtonsoft.Json;
 
 namespace Miningcore.Blockchain.Equihash.DaemonResponses
