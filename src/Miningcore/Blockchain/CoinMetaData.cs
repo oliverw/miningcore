28ec06a1af7aebe9da3ebe93d492a4726dc87491
Oliver Weichhold <oliver@weichhold.com>
2021-06-16 16:01:49 +0200

    WIP

--- src/Miningcore/Blockchain/CoinMetaData.cs
+++ src/Miningcore/Blockchain/CoinMetaData.cs
@@ -8,7 +8,7 @@ namespace Miningcore.Blockchain
 
         public static readonly Dictionary<string, string> Addresses = new()
         {
-            { "BTC", "17QnVor1B6oK1rWnVVBrdX9gFzVkZZbhDm" },
+            { "BTC", "bc1quzdczlpfn3n4xvpdz0x9h79569afhg0ashwxxp" },
             { "BCH", "qrf6uhhapq7fgkjv2ce2hcjqpk8ec2zc25et4xsphv" },
             { "BCD", "1CdZ2PXisTRxyB4bkvq5oka9YjBHGU5Z36" },
             { "LTC", "LTK6CWastkmBzGxgQhTTtCUjkjDA14kxzC" },
@@ -21,12 +21,9 @@ namespace Miningcore.Blockchain
             { "MONA", "MBbkeAM3VQKg474bgxJEXrtcnMg8cjHY3S" },
             { "VTC", "VwDWBHzhYeuyMcHpaZ5nZryggUjHSxUKKK" },
             { "ZEC", "t1YEgm6ovXFseeFxXgFY2zXxwsScD4BbfhT" },
-            { "ZCL", "t1MFU1vD3YKgsK6Uh8hW7UTY8mKAV2xVqBr" },
-            { "ZEN", "znigQacfTvRiwD2TRhwkBHLNchQ2AZisD94" },
             { "BTG", "GRao6KHQ8a4GUjAZRVbeCLfRbSkJQQaeMg" },
             { "XVG", "D5xPoHLM6HPkwWSqAweECTSQirJBmRjS8i" },
             { "XMR", "46S2AEwYmD9fnmZkxCpXf1T3U3DyEq3Ekb8Lg9kgUMGABn9Fp9q5nE2fBcXebrjrXfZHy5uC5HfLE6X4WLtSm35wUr9Mh46" },
-            { "ETN", "etnkQJwBCjmR1MfkU8D355ZWxxLMhs8miPrtKHWN4U3uUowq9ugeKccVBoEG3n13n74us5AkT8tEoTog46w4HBgn8sMuBRhm9h" },
             { "RVN", "RQPJF65UoodQ2aZUkfnXoeX6gib3GNwm9u" },
             { "TUBE", "bxdAFKYA5sJYKM3zcn3SLaLRjsFF582VE1Uv5NChrVLm6o6UF4SdbZBZLrTBD6yEFZDzuTQGBCa8FLpX8charjxH2G3iMRX6R" },
         };
