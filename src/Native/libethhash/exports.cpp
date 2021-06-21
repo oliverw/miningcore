d20c7728b3dfbccefbadfe60537d8c43cdcd0e06
Oliver Weichhold <oliver@weichhold.com>
2021-06-22 00:06:02 +0200

    Fix libethhash exports

--- src/Native/libethhash/exports.cpp
+++ src/Native/libethhash/exports.cpp
@@ -15,8 +15,6 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "stdafx.h"
-
 #include "sha3.h"
 #include "internal.h"
 #include "ethash.h"
