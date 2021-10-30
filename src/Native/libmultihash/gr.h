02b47d4d77b31de5c20f30ba41df8e181dc328de
LOUD Mining <39448763+LOUD-Mining@users.noreply.github.com>
2021-10-30 17:41:26 -0400

    Update gr.h

--- src/Native/libmultihash/gr.h
+++ src/Native/libmultihash/gr.h
@@ -1,1 +1,16 @@
+#ifndef GR_H
+#define GR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
 void gr_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
