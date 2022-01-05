3bd017824079a26c2dda2af8d3f8ac91c7f583cb
Oliver Weichhold <oliver@weichhold.com>
2022-01-06 00:43:52 +0100

    Miner Reports

--- src/Miningcore/Persistence/Repositories/IShareRepository.cs
+++ src/Miningcore/Persistence/Repositories/IShareRepository.cs
@@ -14,5 +14,6 @@ public interface IShareRepository
     Task DeleteSharesByMinerAsync(IDbConnection con, IDbTransaction tx, string poolId, string miner);
     Task<double?> GetAccumulatedShareDifficultyBetweenCreatedAsync(IDbConnection con, string poolId, DateTime start, DateTime end);
     Task<MinerWorkerHashes[]> GetHashAccumulationBetweenCreatedAsync(IDbConnection con, string poolId, DateTime start, DateTime end);
+    Task<KeyValuePair<string, double>[]> GetAccumulatedUserAgentShareDifficultyBetweenCreatedAsync(IDbConnection con, string poolId, DateTime start, DateTime end, bool byVersion = false);
     Task<string[]> GetRecentyUsedIpAddresses(IDbConnection con, IDbTransaction tx, string poolId, string miner);
 }
