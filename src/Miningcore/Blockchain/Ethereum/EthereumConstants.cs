1a7474e5b309c1cf5685134bb3b091105ef01fb2
Oliver Weichhold <oliver@weichhold.com>
2022-09-29 09:38:41 +0200

    Cleanup

--- src/Miningcore/Blockchain/Ethereum/EthereumConstants.cs
+++ src/Miningcore/Blockchain/Ethereum/EthereumConstants.cs
@@ -19,16 +19,13 @@ public class EthereumConstants
     public const string BlockTypeUncle = "uncle";
 
 #if !DEBUG
-        public const int MinPayoutPeerCount = 1;
+    public const int MinPayoutPeerCount = 1;
 #else
     public const int MinPayoutPeerCount = 1;
 #endif
 
     public static readonly Regex ValidAddressPattern = new("^0x[0-9a-fA-F]{40}$", RegexOptions.Compiled);
     public static readonly Regex ZeroHashPattern = new("^0?x?0+$", RegexOptions.Compiled);
-    public static readonly Regex NoncePattern = new("^0x[0-9a-f]{16}$", RegexOptions.Compiled);
-    public static readonly Regex HashPattern = new("^0x[0-9a-f]{64}$", RegexOptions.Compiled);
-    public static readonly Regex WorkerPattern = new("^[0-9a-zA-Z-_]{1,64}$", RegexOptions.Compiled);
 
     public const ulong ByzantiumHardForkHeight = 4370000;
     public const ulong ConstantinopleHardForkHeight = 7280000;
