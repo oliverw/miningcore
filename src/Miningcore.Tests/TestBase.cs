2fd7e1499d422724d30d02067cb099b03f90abde
Oliver Weichhold <oliver@weichhold.com>
2022-10-10 19:54:34 +0200

    BitcoinJob tests

--- src/Miningcore.Tests/TestBase.cs
+++ src/Miningcore.Tests/TestBase.cs
@@ -1,3 +1,6 @@
+using Autofac;
+using Newtonsoft.Json;
+
 namespace Miningcore.Tests;
 
 public abstract class TestBase
@@ -5,5 +8,11 @@ public abstract class TestBase
     protected TestBase()
     {
         ModuleInitializer.Initialize();
+
+        container = ModuleInitializer.Container;
+        jsonSerializerSettings = container.Resolve<JsonSerializerSettings>();
     }
+
+    protected readonly IContainer container;
+    protected readonly JsonSerializerSettings jsonSerializerSettings;
 }
--- src/Miningcore.Tests/Util/MockMasterClock.cs
+++ src/Miningcore.Tests/Util/MockMasterClock.cs
@@ -8,4 +8,12 @@ public class MockMasterClock : IMasterClock
     public DateTime CurrentTime { get; set; }
 
     public DateTime Now => CurrentTime;
+
+    public static MockMasterClock FromTicks(long value)
+    {
+        return new MockMasterClock
+        {
+            CurrentTime = new DateTime(value, DateTimeKind.Utc)
+        };
+    }
 }
