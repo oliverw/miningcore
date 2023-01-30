c9e439a4a112259c3a0f23bfcaf59e53dac7fecc
jon4hz <me@jon4hz.io>
2023-01-30 19:13:11 +0100

    fix: simpler dag interface

--- src/Miningcore/Crypto/Hashing/Ethash/Abstractions.cs
+++ src/Miningcore/Crypto/Hashing/Ethash/Abstractions.cs
@@ -4,9 +4,6 @@ namespace Miningcore.Crypto.Hashing.Ethash;
 
 public interface IEthashDag : IDisposable
 {
-    ulong Epoch { get; set; }
-    DateTime LastUsed { get; set; }
-    Task GenerateAsync(string dagDir, ulong dagEpochLength, ILogger logger, CancellationToken ct);
     bool Compute(ILogger logger, byte[] hash, ulong nonce, out byte[] mixDigest, out byte[] result);
 }
 
