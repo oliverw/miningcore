5c4626576dc6ee8bcd7e1c4edb484b6af19a1d05
itrailmpool <132821956+itrailmpool@users.noreply.github.com>
2023-07-17 23:57:41 +0300

    feat: retrive full worker name from input string

--- src/Miningcore/Blockchain/Bitcoin/BitcoinPool.cs
+++ src/Miningcore/Blockchain/Bitcoin/BitcoinPool.cs
@@ -102,7 +102,11 @@ public class BitcoinPool : PoolBase
         // extract worker/miner
         var split = workerValue?.Split('.');
         var minerName = split?.FirstOrDefault()?.Trim();
-        var workerName = split?.Skip(1).FirstOrDefault()?.Trim() ?? string.Empty;
+        var workerName = string.Empty;
+        if (split.Length >= 2)
+        {
+            workerName = string.Join(".", split.Skip(1))?.Trim() ?? string.Empty();
+        }
 
         // assumes that minerName is an address
         context.IsAuthorized = await manager.ValidateAddressAsync(minerName, ct);
